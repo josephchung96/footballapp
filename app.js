9510bc26f9441aba00a8d6b7f8cfd3e35151de27
wymanchung <wymanchungml@gmail.com>
2017-03-17 01:18:23 +0000

    Part 1: Searching done

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Twit = require('twit');
+var client = new Twit({
+  consumer_key: '5Sqzd11fpZ94Z33NtlXjU4b2W',
+  consumer_secret: 'PFsR1I1gtU0vwADTunZPzkgteOobvSLmCZQYhgnups3weHOUJy',
+  access_token: '1860715854-o7iTu0wVqd9jSyyS4rLNsmjq6CJSKd7xvwsnoBV',
+  access_token_secret: '4fJYsMv2Xk59cxHzHjGaG7WOKLqyU4Earh6FNXlWI3Nyy'
+});
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,12 +25,28 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false  }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.post('/postFile', function(req, res){
+  //console.log(JSON.stringify(req.body));
+  var player = req.body.player;
+  var team = req.body.team;
+  var author = req.body.author;
+  
+  client.get('search/tweets', { q: player+' '+team+' since:2011-11-11', count: 10 },
+  function(err, data, response) {
+    for (var indx in data.statuses) {
+    var tweet= data.statuses[indx];
+    console.log('on: ' + tweet.created_at + ' : @' + tweet.user.screen_name + ' : ' + tweet.text+'\n\n');
+    }
+  });
+  res.send(req.body);
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/js/index.js
@@ -0,0 +1,49 @@
+function toggleSearch(id) {
+	var searchBar = document.getElementById(id);
+	$("input#"+id).toggleClass("hide");
+	$("input#"+id).val("");
+}
+
+function validateForm() {
+    var team = document.getElementById('search')['team'].value;
+    var init = team.charAt(0)
+	if (init!='@' && init!='') {
+		alert("Team name must be a Twitter handle");
+        return false;
+	}else{	
+		console.log(JSON.stringify($("#search").serializeObject()));
+		sendAjaxQuery('postFile', JSON.stringify($("#search").serializeObject()));
+	}
+}
+
+function sendAjaxQuery(url, data) {
+	$.ajax({
+		type: 'POST',
+		url: url,
+		data: data,
+		contentType: 'application/json',
+		success: function (data) {
+			alert('success ');			
+		},
+		error: function (xhr, status, error) {
+			alert('err :'+error)
+		}
+	});
+}
+
+
+$.fn.serializeObject = function () {
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function () {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+};

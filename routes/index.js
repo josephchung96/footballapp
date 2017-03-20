725f5784ac8298ac5c905725a01188652902cce9
wymanchung <wymanchungml@gmail.com>
2017-03-20 22:36:43 +0000

    passing var example

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var template = new Array(10).fill(0);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/trends', function(req, res, next){
-  res.render('trends', { title: 'Trends' });
+  res.render('trends', {
+    title: 'Trends',
+    temp: template 
+  });
 })
 router.post('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

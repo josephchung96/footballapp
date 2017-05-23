fcb1b5fd6fc9d81af821d2e8f389a2af5b730085
Declan <declan107@gmail.com>
2017-05-24 00:10:44 +0100

    dbpedia

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var dbcon = require('../config/db.js');
 var client = require('../config/twitter.js');
 var socket_io = require('socket.io');
+var rdf = require('rdf')
+var sparql = require('sparql')
+var sparql_client = require('sparql-client')
+var util = require('util')
+var endpoint = 'http://dbpedia.org/spqrql'
 var currentStream;
 
 /* GET home page. */
@@ -303,7 +308,84 @@ router.post('/postFile', function(req, res){
 			
 		}
     });
-	
+    /*
+    var chelseaScreenName = ['Radamel Falcao','Ruben Loftus Cheek','Stipe Perica',
+                             'Branislav Ivanovic','Lucas Piazon','Mario Pasalic',
+                             'Todd Kane','Matt Miazga','Joao Rodriguez',
+                             'Charly Musonda Jr','John Terry','Marco Amelia',
+                             'Oscar','Gary Cahill','Marco Van Ginkel',
+                             'Lewis Baker','Matej Delac','Asmir Begovic',
+                             'John Swift','Andreas Christensen','Baba Rahman',
+                             'Bertrand Traore','Christian Atsu','Dom Solanke',
+                             'Eden Hazard','Isaiah Brown','Jamal Blackman',
+                             'Jeremie Boga','Michael Hector','Mitchell Beeney',
+                             'Nathan','Nathan Ake','Nathaniel Chalobah','Papy Djilobodji',
+                             'Patrick Bamford','Victorien Angban','Wallace Oliveira',
+                             'Pato','Danilo Pantic','Juan Cuadrado','Tomas Kalas',
+                             'Marko Marin','Cesc Fabregas Soler','Thibaut Courtois','Victor Moses',
+                             'Diego Costa','Cristian Cuevas','Mikel John Obi','Pedro Rodriguez',
+                             'Robery Kenedy','Nemanja Matic','Loic Remy','Cesar Azpilicueta',
+                             'Kurt Zouma','Willian','Kenneth Omeruo']
+    var manutdScreenName = ['Anthony Martial','Andreas Pereira','Luke Shaw','Marouane Fellaini','Ander Herrera',
+                            'Daley Blind','Joe Riley','Borthwick Jackson','Chris Smalling','Joe Rothwell',
+                            'Joel Castro Pereira','Marcos Rojo','Marcus Rashford','Phil Jones','Sam Johnstone',
+                            'Timothy Fosu Mensah','Victor Valdes','Matteo Darmian','Tyler Blackett','Regan Poole',
+                            'James Weir','Memphis Depay','Will Keane','Adnan Januzaj','Michael Carrick',
+                            'Morgan Schneiderlin','David De Gea','James Wilson','Basti Schweinsteiger',
+                            'Antonio Valencia','Sergio Romero','Guillermo Varela','Juan Mata Garcia','Nick Powell',
+                            'Donald Love','Wayne Rooney','Ashley Young']
+    var chelseaDbpedia = ['http://dbpedia.org/resource/Radamel_Falcao','http://dbpedia.org/resource/Ruben_Loftus-Cheek',
+                          'http://dbpedia.org/resource/Stipe_Perica','http://dbpedia.org/resource/Branislav_Ivanovi%C4%87',
+                          'http://dbpedia.org/resource/Lucas_Piazon','http://dbpedia.org/resource/Mario_Pa%C5%A1ali%C4%87','http://dbpedia.org/resource/Todd_Kane',
+                          'http://dbpedia.org/resource/Matt_Miazga','http://dbpedia.org/resource/Joao_Rodr%C3%ADguez_(footballer)',
+                          'http://dbpedia.org/resource/Charly_Musonda_(footballer,_born_1996)','http://dbpedia.org/resource/John_Terry',
+                          'http://dbpedia.org/resource/Marco_Amelia','http://dbpedia.org/resource/Oscar_(footballer,_born_1991)',
+                          'http://dbpedia.org/resource/Gary_Cahill','http://dbpedia.org/resource/Marco_van_Ginkel','http://dbpedia.org/resource/Lewis_Baker_(footballer)',
+                          'http://dbpedia.org/resource/Matej_Dela%C4%8D','http://dbpedia.org/resource/Asmir_Begovi%C4%87'
+                          'http://dbpedia.org/resource/John_Swift_(footballer,_born_1995)','http://dbpedia.org/resource/Andreas_Christensen',
+                          'http://dbpedia.org/resource/Baba_Rahman','http://dbpedia.org/resource/Bertrand_Traor%C3%A9',
+                          'http://dbpedia.org/resource/Christian_Atsu','http://dbpedia.org/resource/Dominic_Solanke',
+                          'http://dbpedia.org/resource/Eden_Hazard','http://dbpedia.org/resource/Isaiah_Brown',
+                          'http://dbpedia.org/resource/Jamal_Blackman','http://dbpedia.org/resource/J%C3%A9r%C3%A9mie_Boga',
+                          'http://dbpedia.org/resource/Michael_Hector','http://dbpedia.org/resource/Mitchell_Beeney',
+                          'http://dbpedia.org/resource/Nathan_(footballer,_born_1996)','http://dbpedia.org/resource/Nathan_Ak%C3%A9',
+                          'http://dbpedia.org/resource/Nathaniel_Chalobah','http://dbpedia.org/resource/Papy_Djilobodji',
+                          'http://dbpedia.org/resource/Patrick_Bamford','http://dbpedia.org/resource/Victorien_Angban',
+                          'http://dbpedia.org/resource/Wallace_Oliveira','http://dbpedia.org/resource/Alexandre_Pato',
+                          'http://dbpedia.org/resource/Danilo_Panti%C4%87','http://dbpedia.org/resource/Juan_Cuadrado',
+                          'http://dbpedia.org/resource/Tom%C3%A1%C5%A1_Kalas','http://dbpedia.org/resource/Cesc_F%C3%A0bregas',
+                          'http://dbpedia.org/resource/Thibaut_Courtois','http://dbpedia.org/resource/Victor_Moses',
+                          'http://dbpedia.org/resource/Diego_Costa','http://dbpedia.org/resource/Cristi%C3%A1n_Cuevas',
+                          'http://dbpedia.org/resource/John_Obi_Mikel','http://dbpedia.org/resource/Pedro_(footballer,_born_July_1987)',
+                          'http://dbpedia.org/resource/Kenedy_(footballer)','http://dbpedia.org/resource/Nemanja_Mati%C4%87',
+                          'http://dbpedia.org/resource/Lo%C3%AFc_R%C3%A9my','http://dbpedia.org/resource/C%C3%A9sar_Azpilicueta',
+                          'http://dbpedia.org/resource/Kurt_Zouma','http://dbpedia.org/resource/Willian_(footballer)','http://dbpedia.org/resource/Kenneth_Omeruo']
+    var manutdDbpedia =  ['http://dbpedia.org/resource/Anthony_Martial','http://dbpedia.org/resource/Andreas_Pereira','http://dbpedia.org/resource/Luke_Shaw',
+                          'http://dbpedia.org/resource/Marouane_Fellaini','http://dbpedia.org/resource/Paddy_McNair','http://dbpedia.org/resource/Jesse_Lingard',
+                          'http://dbpedia.org/resource/Ander_Herrera','http://dbpedia.org/resource/Daley_Blind','http://dbpedia.org/resource/Joe_Riley_(footballer,_born_1996)',
+                          'http://dbpedia.org/resource/Cameron_Borthwick-Jackson','http://dbpedia.org/resource/Chris_Smalling','http://dbpedia.org/resource/Joe_Rothwell',
+                          'http://dbpedia.org/resource/Joel_Castro_Pereira','http://dbpedia.org/resource/Marcos_Rojo','http://dbpedia.org/resource/Marcus_Rashford',
+                          'http://dbpedia.org/resource/Phil_Jones_(footballer,_born_1992)','http://dbpedia.org/resource/Sam_Johnstone','http://dbpedia.org/resource/Timothy_Fosu-Mensah',
+                          'http://dbpedia.org/resource/V%C3%ADctor_Vald%C3%A9s','http://dbpedia.org/resource/Matteo_Darmian','http://dbpedia.org/resource/Tyler_Blackett',
+                          'http://dbpedia.org/resource/Regan_Poole','http://dbpedia.org/resource/James_Weir_(footballer)','http://dbpedia.org/resource/Memphis_Depay',
+                          'http://dbpedia.org/resource/Will_Keane','http://dbpedia.org/resource/Adnan_Januzaj','http://dbpedia.org/resource/Michael_Carrick',
+                          'http://dbpedia.org/resource/Morgan_Schneiderlin','http://dbpedia.org/resource/David_de_Gea','http://dbpedia.org/resource/James_Wilson_(footballer,_born_1995)',
+                          'http://dbpedia.org/resource/Bastian_Schweinsteiger','http://dbpedia.org/resource/Antonio_Valencia','http://dbpedia.org/resource/Juan_Mata',
+                          'http://dbpedia.org/resource/Nick_Powell','http://dbpedia.org/resource/Donald_Love',
+                          'http://dbpedia.org/resource/Wayne_Rooney','http://dbpedia.org/resource/Ashley_Young']
+    for (var i = 0; i <=  chelseaScreenName.length; i++)
+        var key1 = SEARCHWORD.replace(/\s/g, '').toLowerCase();
+        var key2 = chelseaScreenName[k].replace(/\s/g, '').toLowerCase();
+        var key3 = manutdScreenName[k].replace(/\s/g, '').toLowerCase();
+        if (key1 == key2)
+          var query = 'SELECT ?name ?position ?dob FROM ' + chelseaDbpedia[k] + 'WHERE {dbp.name ?name;  dbo.position ?position; dbo.birthDate ?dob }';
+        if (key1 == key3)
+          var query = 'SELECT ?name ?position ?dob FROM ' + manutdDbpedia[k] + 'WHERE {dbp.name ?name;  dbo.position ?position; dbo.birthDate ?dob }';
+        if (key1 != key2 && key1 != key3)
+    var client = new SparqlClient(endpoint);
+    client.query(query)
+    */
+    
 // passing variables to route
 	req.app.set('tweetData',tweetData);
 	req.app.set('lastWeekCount',lastWeekCount);
